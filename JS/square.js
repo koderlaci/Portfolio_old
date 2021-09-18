91737262faa6fed70fe314b8cae78ac823e207eb
koderlaci <koder.laci67@gmail.com>
2021-09-18 14:15:02 +0200

    JS - cmd kurzor elkészítése

---
+++ JS/square.js
@@ -0,0 +1,15 @@
+var i = 2;
+
+function Visible(){
+    if(i % 2 == 0)
+    {
+        document.getElementById('square').style.visibility = "hidden";
+    }
+    else
+    {
+        document.getElementById('square').style.visibility = "visible";
+    }
+    i++;
+    
+}
+window.onload = setInterval(Visible, 500);
