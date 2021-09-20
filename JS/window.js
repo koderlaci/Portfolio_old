f47d45b570b2b3b7149f81de22bc3c41a283b247
koderlaci <koder.laci67@gmail.com>
2021-09-20 17:03:52 +0200

    windows.js ablakvezérlő ikonok eventlistenerek elkészítése

--- JS/window.js
+++ JS/window.js
@@ -15,3 +15,125 @@ let ppExit = document.querySelector(".icon-pp-exit");
 let ppMinimize = document.querySelector(".icon-pp-minimize");
 let ppSendTray = document.querySelector(".icon-pp-sendTray");
 let ppTray = document.querySelector(".tray-pp");
+
+cmdExit.addEventListener("click", ()=> {
+    alert("Ezt az ablakot nem zárhatod be!");
+});
+
+cmdMinimize.addEventListener("click", ()=> {
+    if(cmd.style.height == "100%")
+    {
+        cmd.style.left = "5%";
+        cmd.style.top = "5%";
+        cmd.style.height = "60%";
+        cmd.style.width = "50%";
+    }
+    else
+    {
+        cmd.style.left = 0;
+        cmd.style.top = 0;
+        cmd.style.height = "100%";
+        cmd.style.width = "100%";
+    }  
+});
+
+cmdSendTray.addEventListener("click", ()=>{
+    cmd.style.visibility = "hidden";
+    document.querySelector(".tray-cmd").style.backgroundColor = "rgb(58, 58, 51)";
+    document.querySelector("#square").innerHTML = "";
+});
+
+cmdTray.addEventListener("click", ()=>{
+    if(cmd.style.visibility == "hidden")
+    {
+        cmd.style.visibility = "visible";
+        document.querySelector(".tray-cmd").style.backgroundColor = "rgb(175, 171, 171)";
+        document.querySelector("#square").innerHTML = "█";
+    }
+    else
+    {
+        cmd.style.visibility = "hidden";
+        document.querySelector(".tray-cmd").style.backgroundColor = "rgb(58, 58, 51)";
+        document.querySelector("#square").innerHTML = "";
+    }
+});
+
+wordExit.addEventListener("click", ()=> {
+    intro.style.visibility = "hidden";
+    wordTray.style.visibility = "hidden";
+    wordTray.style.display = "none";
+});
+
+wordMinimize.addEventListener("click", ()=> {
+    if(intro.style.height == "95%")
+    {
+        intro.style.height = "80%";
+        intro.style.width = "80%";
+        intro.style.top = "10%";
+    }
+    else
+    {
+        intro.style.height = "95%";
+        intro.style.width = "100%";
+        intro.style.top = 0;
+        document.querySelector(".screen-word").style.border = "0px";
+    }  
+});
+
+wordSendTray.addEventListener("click", ()=>{
+    intro.style.visibility = "hidden";
+    document.querySelector(".tray-word").style.backgroundColor = "rgb(58, 58, 51)";
+});
+
+wordTray.addEventListener("click", ()=>{
+    if(intro.style.visibility == "visible")
+    {
+        intro.style.visibility = "hidden";
+        document.querySelector(".tray-word").style.backgroundColor = "rgb(58, 58, 51)";
+    }
+    else
+    {
+        intro.style.visibility = "visible";
+        document.querySelector(".tray-word").style.backgroundColor = "rgb(175, 171, 171)";
+    }
+});
+
+ppExit.addEventListener("click", ()=> {
+    pp.style.visibility = "hidden";
+    ppTray.style.visibility = "hidden";
+});
+
+ppMinimize.addEventListener("click", ()=> {
+    if(pp.style.height == "100%")
+    {
+        pp.style.left = "10%";
+        pp.style.top = "10%";
+        pp.style.height = "80%";
+        pp.style.width = "80%";
+    }
+    else
+    {
+        pp.style.left = 0;
+        pp.style.top = 0;
+        pp.style.height = "100%";
+        pp.style.width = "100%";
+    }  
+});
+
+ppSendTray.addEventListener("click", ()=>{
+    pp.style.visibility = "hidden";
+    document.querySelector(".tray-pp").style.backgroundColor = "rgb(58, 58, 51)";
+});
+
+ppTray.addEventListener("click", ()=>{
+    if(pp.style.visibility == "visible")
+    {
+        pp.style.visibility = "hidden";
+        document.querySelector(".tray-pp").style.backgroundColor = "rgb(58, 58, 51)";
+    }
+    else
+    {
+        pp.style.visibility = "visible";
+        document.querySelector(".tray-pp").style.backgroundColor = "rgb(175, 171, 171)";
+    }
+});
