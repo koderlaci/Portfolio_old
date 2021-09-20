22e73bedc45201d055ae24319a4f36643b4b6383
koderlaci <koder.laci67@gmail.com>
2021-09-20 16:27:29 +0200

    window.js html elemek deklarálása

---
+++ JS/window.js
@@ -0,0 +1,17 @@
+let cmd = document.querySelector("#cmd");
+let cmdExit = document.querySelector(".icon-cmd-exit");
+let cmdMinimize = document.querySelector(".icon-cmd-minimize");
+let cmdSendTray = document.querySelector(".icon-cmd-sendTray");
+let cmdTray = document.querySelector(".tray-cmd");
+
+let intro = document.querySelector("#introduction");
+let wordExit = document.querySelector(".icon-exit");
+let wordMinimize = document.querySelector(".icon-minimize");
+let wordSendTray = document.querySelector(".icon-sendTray");
+let wordTray = document.querySelector(".tray-word");
+
+let pp = document.querySelector("#experiences");
+let ppExit = document.querySelector(".icon-pp-exit");
+let ppMinimize = document.querySelector(".icon-pp-minimize");
+let ppSendTray = document.querySelector(".icon-pp-sendTray");
+let ppTray = document.querySelector(".tray-pp");
