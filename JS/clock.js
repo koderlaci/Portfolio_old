ba92116c9ac435c836b79a20492db3ddaf046091
koderlaci <koder.laci67@gmail.com>
2021-09-23 14:10:51 +0200

    index | clock.js | style - clock javítása és notifications ikon hozzáadása

--- JS/clock.js
+++ JS/clock.js
@@ -4,6 +4,9 @@ function Clock(){
     let hours = clock.getHours();
     let minutes = clock.getMinutes();
 
+    if (hours < 10) hours = "0" + hours;
+    if (minutes < 10) minutes = "0" + minutes;
+
     document.querySelector(".clock").innerHTML = hours + ":" + minutes;
 
     setTimeout(Clock, 1000);
